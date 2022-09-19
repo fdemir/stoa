37cca029285bff665b148a2ab9a568b5edeb5e3f
fdemir <frndemir@gmail.com>
2022-09-19 20:21:58 +0300

    :wrench: addd dev config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+const devConfig = defineConfig({
+  plugins: [react()],
+});
+
+export default devConfig;
